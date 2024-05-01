7447e983ac68cd09c20827872076bae5110c7d49
Elhamirzaheidar <emirzaheidar@gmail.com>
2024-04-30 23:44:20 -0700

    NotFoundDetail

--- src/features/authentication/NotFoundPage.jsx
+++ src/features/authentication/NotFoundPage.jsx
@@ -10,8 +10,8 @@ function NotFoundPage() {
           <h1 className="text-xl font-bold text-gray-400 mb-8">
             صفحه ای که دنبالش بودید،پیدانشد{" "}
           </h1>
-          <button onClick={navigate}>
-            <HiArrowRight className="w-6 h-6 text-gray-900" />
+          <button onClick={navigate} className="flex justify-normal gap-1">
+            <HiArrowRight className="w-6 h-6 text-blue-600" />
             <span>برگشت</span>
           </button>
         </div>
