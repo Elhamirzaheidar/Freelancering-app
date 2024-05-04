0475266bb3ba3bf5e0379b3d915ab5c776057250
Elhamirzaheidar <emirzaheidar@gmail.com>
2024-05-03 23:22:37 -0700

    useOwnerProject

--- src/features/Projects/projectTable.jsx
+++ src/features/Projects/projectTable.jsx
@@ -1,7 +1,11 @@
+import Empty from "../../ui/Empty";
+import Loading from "../../ui/Loading";
 import useOwnerProjects from "./useOwnerProjects"
 
 function ProjectTable() {
     const{isLoading,projects}=useOwnerProjects();
+if(isLoading) return <Loading/>
+if(!projects.length) return <Empty resourceName="پروژه"/>
   return (
     <div>projectTable</div>
   )
--- src/services/ProjectServices.js
+++ src/services/ProjectServices.js
@@ -1,5 +1,4 @@
 import http from "./Httpservice"
 
-export default getOwnerProjectApi(){
-    return http.get("/project/owner-projects").then(({data})=>data.data);
-}
+export function getOwnerProjectApi(){
+    return (http.get("/project/owner-projects").then(({data})=>data.data));}
